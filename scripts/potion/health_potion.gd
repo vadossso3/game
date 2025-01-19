4dd73b7c270cf62f96b56f9bebf68325ea53ada5
ChnakIgor <49717732+ChnakIgor@users.noreply.github.com>
2025-01-19 21:06:03 +0300

    item pick up sounds

--- scripts/potion/health_potion.gd
+++ scripts/potion/health_potion.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@onready var animation_player = $AnimationPlayer
 var heal = 20
 
 signal healPlayer(heal)
@@ -11,4 +12,4 @@ func _on_area_entered(body) -> void:
 	if body is CharacterBody2D:
 		if body.health != body.max_health:
 			emit_signal("healPlayer", heal)
-			queue_free()
+			animation_player.play("pickup")
--- scripts/potion/posion_potion.gd
+++ scripts/potion/posion_potion.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @export var damage_per_second: int = 10
 @export var duration: float = 3.0
+@onready var animation_player = $AnimationPlayer
+@onready var pickup_sound =$AudioStreamPlayer
 
 var timer: Timer
 var elapsed_time: float = 0.0
@@ -13,12 +15,12 @@ func _ready() -> void:
 	body_entered.connect(_on_area_entered)
 	
 func start_poison():
-	hide()
 	timer.start()
 	elapsed_time = 0.0
 
 func _on_area_entered(body) -> void:
 	if body is CharacterBody2D:
+		animation_player.play("pickup")
 		start_poison()
 
 func _on_damage_timer_timeout() -> void:
@@ -27,4 +29,3 @@ func _on_damage_timer_timeout() -> void:
 	
 	if elapsed_time >= duration:
 		timer.stop()
-		queue_free()
