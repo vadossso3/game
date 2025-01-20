30bfbbc22e89c3b987ad937f0a1ba2c5cc9964ed
ChnakIgor <49717732+ChnakIgor@users.noreply.github.com>
2025-01-20 23:09:18 +0300

    add boombox asset + animation

--- scripts/environment/furniture/music_center.gd
+++ scripts/environment/furniture/music_center.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 signal music_interacted(audio_player, is_playing)
 
 @onready var audio_player : AudioStreamPlayer2D = $AudioStreamPlayer2D
@@ -11,7 +13,9 @@ func interact():
 	if is_playing:
 		paused_position = audio_player.get_playback_position()
 		audio_player.stop()
+		animated_sprite.play("stop")
 		is_playing = false
 	else:
 		audio_player.play(paused_position)
+		animated_sprite.play("playing")
 		is_playing = true
